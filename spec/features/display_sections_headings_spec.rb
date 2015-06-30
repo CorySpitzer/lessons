e61d342eaf1375b746602c1e951d8039cd8c9c04
Cory Spitzer & Jessica Engel <student@epicodus.com>
2015-06-30 11:05:24 -0700

    Display sections starting at 1

--- spec/features/display_sections_headings_spec.rb
+++ spec/features/display_sections_headings_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'displaying sections on homepage' do
   it 'displays section headings on homepage' do
-    visit root_path
-    #TODO: populate db with form
-    # expect(page).to have_content 'Section 1'
+    visit new_section_path
+    fill_in 'section_heading', :with => 'Javascript'
+    click_on 'Create Section'
+    expect(page).to have_content 'Section 1'
   end
 end
