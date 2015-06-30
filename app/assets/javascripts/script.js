36579c43de9373575cd3cd3ca13849cbeb6b37d6
Cory Spitzer & Jessica Engel <student@epicodus.com>
2015-06-30 14:28:26 -0700

    Make slidedowns bounce six times on refresh

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,8 +1,8 @@
 
-$(function() {
+$(document).on('DOMNodeInserted', function() {
   $('.title').on('click', function() {
     $('.hidden-content').slideUp("fast");
     var firstWord = this.textContent.split(' ')[0];
     $('.' + firstWord).slideDown("fast");
   });
-})
+});
