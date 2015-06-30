62767f6889ca7a6f8e4e0fb4a3d2631d20f829a3
Cory Spitzer & Jessica Engel <student@epicodus.com>
2015-06-30 12:06:50 -0700

    Display lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -5,4 +5,19 @@ class LessonsController < ApplicationController
     @lesson = @section.lessons.new
   end
 
+  def create
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.new(lesson_params)
+    if @lesson.save
+      redirect_to section_path(@section.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:title, :content)
+  end
+
 end
--- spec/features/add_lesson_form_page_spec.rb
+++ spec/features/add_lesson_form_page_spec.rb
@@ -10,10 +10,12 @@ describe 'the new lesson path' do
   # lesson_test = section_test.lessons.create({title: 'Javascript', content: 'Learning'})
 
 
-  # it 'creates a new lesson' do
-  #   visit new_lesson_path
-  #   fill_in 'section_heading', :with => 'Javascript'
-  #   click_on 'Create Section'
-  #   expect(page).to have_content 'Javascript'
-  # end
+  it 'creates a new lesson' do
+    section = Section.create({heading: 'Programming'})
+    visit new_section_lesson_path(section.id)
+    fill_in 'lesson_title', :with => 'Javascript'
+    fill_in 'lesson_content', :with => 'Put semicolons;'
+    click_on 'Create Lesson'
+    expect(page).to have_content 'Put semicolons;'
+  end
 end
