9b897ccb75a51fe3a38afd14c41cee547ac5cd27
Cory Spitzer & Jessica Engel <student@epicodus.com>
2015-06-30 16:41:13 -0700

    Always go to a valid lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,28 @@
 class Lesson < ActiveRecord::Base
+  def self.all_id
+    lessons = Lesson.all
+    ids = []
+    lessons.each do |lesson|
+      ids << lesson.id
+    end
+    ids
+  end
+
+  def next_valid_id
+    if Lesson.all_id.include?(self.id + 1)
+      return id + 1
+    else
+      return 1
+    end
+  end
+
+  def previous_valid_id
+    if Lesson.all_id.include?(self.id - 1)
+      return id - 1 
+    else
+      return 1
+    end
+  end
   validates_presence_of :title
   validates_presence_of :content
   belongs_to :section
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -4,4 +4,13 @@ describe Lesson do
   it { should validate_presence_of :title}
   it { should validate_presence_of :content}
   it { should belong_to :section}
+
+  describe "#next_valid_id" do
+    it "returns the next id" do
+      section = Section.create heading: 'programming'
+      lesson1 = section.lessons.create title: "C", content: "Stuff"
+      lesson2 = section.lessons.create title: "B", content: "Old Stuff"
+      expect(lesson1.next_valid_id).to eq lesson2.id
+    end
+  end
 end
